8f2747e3fb7ca6eeb995605bc86bb37c289656b8
infinity1224 <148772232+infinity1224@users.noreply.github.com>
2024-12-10 22:09:59 +0530

    Create aws_resource_tracker.sh

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#############################################
+#Author: Azmathulla G
+#Version: v1
+#Date: 09-12-2024
+#This script outputs the aws resources usage
+#############################################
