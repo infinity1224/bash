a36c8f7034089089fe88eb6b37fd8057b3601366
infinity1224 <148772232+infinity1224@users.noreply.github.com>
2024-12-10 22:51:20 +0530

    Update aws_resource_tracker.sh

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-#############################################
+#################################################
 #Author: Azmathulla G
 #Version: v1
 #Date: 09-12-2024
-#This script outputs the aws resources usage
-#############################################
+#This script will report the aws resources usage
+#################################################
+
+set -x #debug mode
+
+#AWS S3
+#AWS EC2
+#AWS Lambda
+#AWS IAM Users
+
+#list s3 buckets
+aws s3 ls >> resources.txt
+
+#list EC2 instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> resources.txt
+
+#list lambda
+aws lambda list-functions >> resources.txt
+
+#list users
+aws iam list-users >> resources.txt
